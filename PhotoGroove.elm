6330b1a77962a65f4cc86a4a391aa5f4009c5cbe
Paolo D'Incau <paolo.dincau@gmail.com>
2017-03-15 16:19:43 +0100

    initial commit

---
+++ PhotoGroove.elm
@@ -0,0 +1,24 @@
+module PhotoGroove exposing (...)
+
+import Html exposing  (...)
+import Html.Attributes exposing (...)
+
+urlPrefix = "http://elm-in-action.com/"
+
+initialModel =
+    [ { url = "1.jpeg" }
+    , { url = "2.jpeg" }
+    , { url = "3.jpeg" }
+    ]
+
+view model =
+    div [ class "content" ]
+        [ h1 [] [ text "Photo Groove" ]
+        , div [ id "thumbnail" ] ( List.map viewThumbnail model )
+        ]
+
+viewThumbnail thumbnail =
+    img [ src (urlPrefix ++ thumbnail.url) ] []
+
+main =
+    view initialModel
