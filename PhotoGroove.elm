3685ab72d19b84dd34c7b5f0582937e889548c70
Paolo D'Incau <paolo.dincau@gmail.com>
2017-03-19 20:27:47 +0100

    adding thumbnail selection to get preview

--- PhotoGroove.elm
+++ PhotoGroove.elm
@@ -6,19 +6,30 @@ import Html.Attributes exposing (..)
 urlPrefix = "http://elm-in-action.com/"
 
 initialModel =
-    [ { url = "1.jpeg" }
-    , { url = "2.jpeg" }
-    , { url = "3.jpeg" }
-    ]
+    { photos =
+        [ { url = "1.jpeg" }
+        , { url = "2.jpeg" }
+        , { url = "3.jpeg" }
+        ]
+    , selectedUrl = "1.jpeg"
+    }
 
 view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
-        , div [ id "thumbnail" ] ( List.map viewThumbnail model )
+        , div [ id "thumbnail" ] ( List.map (viewThumbnail model.selectedUrl) model.photos )
+        , img [
+            class "large"
+            , src (urlPrefix ++ "large/" ++ model.selectedUrl)
+            ]
+            []
         ]
 
-viewThumbnail thumbnail =
-    img [ src (urlPrefix ++ thumbnail.url) ] []
+viewThumbnail selectedUrl thumbnail =
+    img [ src (urlPrefix ++ thumbnail.url)
+    , classList [ ("selected", selectedUrl == thumbnail.url ) ]
+    ]
+    []
 
 main =
     view initialModel
